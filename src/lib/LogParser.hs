249d83f5e49ccfa8a50b8b7f47a39223f4cd195f
Raphael Mader <RaphMad@users.noreply.github.com>
2016-01-29 22:09:19 +0100

    Added parsing for date / time

--- src/lib/LogParser.hs
+++ src/lib/LogParser.hs
@@ -1,4 +1,41 @@
 module LogParser where
 
+import Data.Attoparsec.ByteString.Char8
+import Data.Ratio
+import Data.Time
+
 theTruth :: Integer
 theTruth = 42
+
+period :: Parser Char
+period = char '.'
+
+colon :: Parser Char
+colon = char ':'
+
+comma :: Parser Char
+comma = char ','
+
+date :: Parser Day
+date = do
+   day  <- decimal
+   period
+   month <- decimal
+   period
+   year  <- decimal
+   return $ fromGregorian year month day
+
+time :: Parser TimeOfDay
+time = do
+   hour  <- decimal
+   colon
+   minute  <- decimal
+   colon
+   second  <- decimal
+   comma
+   milli <- decimal
+   let totalmilli = (second * 1000 + milli) % 1000
+   return $ TimeOfDay hour minute (fromRational totalmilli)
+
+timeStamp :: Parser LocalTime
+timeStamp = LocalTime <$> date <* space <*> time
--- src/test/LogParserSpec.hs
+++ src/test/LogParserSpec.hs
@@ -1,12 +1,43 @@
 module Main where
 
+import Data.ByteString.Char8
+import Data.Attoparsec.ByteString.Char8
 import LogParser
 import Test.Hspec
+import Test.Hspec.Attoparsec
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
 
-   describe "Check arithmetics" $
+   describe "date parser" $ do
 
-   it "adds correctly" $
-      2 + 3 `shouldBe` 5
+      it "succeeds on correct date" $
+         date `succeedsOn` "04.01.2016"
+
+      it "fails on wrong date" $
+         date `failsOn` "04.error.2016"
+
+   describe "time parser" $ do
+
+      it "succeeds on correct time" $
+         time `succeedsOn` "04:04:54,407"
+
+      it "fails on wrong time" $
+         time `failsOn` "04:04error,407"
+
+   describe "timeStamp parser" $ do
+
+      it "succeeds on correct dateTime" $
+         timeStamp `succeedsOn` "04.01.2016 04:04:54,407"
+
+      it "succeeds on correct dateTime with appendix" $
+         timeStamp `succeedsOn` "04.01.2016 04:04:54,407appendix"
+         
+      it "fails on wrong dateTime" $
+         timeStamp `succeedsOn` "04.01.2016error 04:04:54,407"
+
+succeedsOn :: Show a => Parser a -> String -> Expectation
+succeedsOn p s = p `shouldSucceedOn` pack s
+
+failsOn :: Show a => Parser a -> String -> Expectation
+failsOn p s = p `shouldFailOn` pack s
